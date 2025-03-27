ae4a64568c8b6e59d8df164cf857d7a5fc10ec0c
AlexBob <billbob.dong@gmail.com>
2025-03-27 11:21:38 +0800

    添加项目基础结构和初始文件，包括.gitattributes和.gitignore配置、Avalonia UI应用程序的基本文件、许可证文件和README文档。

---
+++ AvaPlate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AvaPlate.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "苏轼\n清风徐来，水波不兴。" +
+                                      "举酒属客，诵明月之诗，歌窈窕之章。少焉，" +
+                                      "月出於东山之上，徘徊於斗牛之间。白露横江，" +
+                                      "水光接天。纵壹苇之所如，淩万顷之茫然。" +
+                                      "浩浩乎如冯虚御风，而不知其所止;飘飘乎如遗世独立，羽化而登仙。";
+}
---
+++ AvaPlate/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaPlate.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
